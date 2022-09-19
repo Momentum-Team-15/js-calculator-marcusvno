9992cf1a79956bcf126d0a17b48b69384b3c8d17
Marcus Nogueira <marcusnogueira23@gmail.com>
2022-09-19 18:15:32 -0400

    Initial commit.

--- main.js
+++ main.js
@@ -1,68 +1,25 @@
-const canvas = document.getElementById("myCanvas");
-const ctx = canvas.getContext("2d");
-const x = canvas.width/2;
-const y = canvas.height-30;
-const dx = 2;
-const dy = -2;
+let squares = document.querySelectorAll(".square");
+// console.log(squares)
+// JS, go find all the elements with the class 'square'
+// querySelector only finds the first element
+// JS gets the literal chunks of HTML
+let frame = document.querySelector("#picture-frame");
+// JS gets the picture frame element
 
+for (let square of squares) {
+  // use a loop to apply the behavior to every square
+  square.addEventListener("click", (event) => {
+    // listen (wait) for the user to click and then do
+    // the stuff inside the {}
 
-const calcRowCount = 4;
-const calcColumnCount = 4;
-const buttonWidth = 100;
-const buttonHeight = 100;
-const buttonPadding = 1;
-const buttonOffsetTop = 10;
-const buttonOffsetLeft = 30;
-
-const buttons = [];
-for(let c=0; c < calcColumnCount; c++){
-    buttons[c]=[];
-    for(let r=0; r < calcRowCount; r++)
-        buttons[c][r] = {x: 0, y:0};
+    // template literal allows us to put variable values in the middle
+    // text
+    square.classList.remove("purple");
+    square.classList.add("teal");
+    if (square.id === "square--one") {
+      console.log(`The ${square.innerText} was clicked`);
+      frame.style.backgroundImage = 'url("http://placekitten.com/300/300")';
+      console.log(frame.style.backgroundImage);
+    }
+  });
 }
-
-
-
-/* function drawCalc(){
-    for(let c=0; c < calcColumnCount; c++)
-        for(let r = 0; r < calcRowCount; r++){
-            const buttonX = c * (buttonWidth + buttonPadding) + buttonOffsetLeft;
-            const buttonY = r * (buttonHeight + buttonPadding) + buttonOffsetTop;
-            buttons[c][r].x = buttonX;
-            buttons[c][r].y = buttonY;
-            ctx.beginPath();
-            ctx.rect(buttonX, buttonY, buttonWidth, buttonHeight);
-            ctx.fillStyle = "#0095DD";
-            ctx.fill();
-            ctx.closePath();
-
-        }
-
-function draw(){
-    drawCalc();
-    
-} */
-
-ctx.beginPath();
-ctx.rect(160, 10, 100, 40);
-ctx.strokeStyle = "rgba(0, 0, 255, 0.5)";
-ctx.stroke();
-ctx.closePath();
-
-
-function drawBall() {
-    ctx.beginPath();
-    ctx.arc(x, y, 10, 0, Math.PI*2);
-    ctx.fillStyle = "#0095DD";
-    ctx.fill();
-    ctx.closePath();
-}
-
-function draw() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    drawBall();
-    x += dx;
-    y += dy;
-}
-
-setInterval(draw,10);
