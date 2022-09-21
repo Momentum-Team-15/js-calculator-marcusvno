0a9514ac0e5c696c01ee510029dbbd122cafebeb
Marcus Nogueira <marcusnogueira23@gmail.com>
2022-09-21 04:29:39 -0400

    Switch added for buttons.

--- main.js
+++ main.js
@@ -7,20 +7,102 @@ let display = document.querySelector("#inputDisplay");
 // JS gets the literal chunks of HTML
 
 
+let displayStorageArray = [0];
 
 
 
-
-for (let i; i< squares.length; i++) {
-  // use a loop to apply the behavior to every square
+for (let square of squares) {
   square.addEventListener("click", (event) => {
-    // listen (wait) for the user to click and then do
-    // the stuff inside the {}
+    switch(square.id){
+      case 'squareZero' :
+        if(displayStorageArray.at(-1) !== 'x' && displayStorageArray.at(-1) !== '-' && displayStorageArray.at(-1) !== "+" && displayStorageArray.at(-1) !== '/' &&  )
+          displayStorageArray.push(0);
+        console.log(displayArray);
+        break;
+      case 'squareOne' :
+        if(displayStorageArray.at(-1) !== 'x' && displayStorageArray.at(-1) !== '-' && displayStorageArray.at(-1) !== "+" && displayStorageArray.at(-1) !== '/' &&  )
+        displayStorageArray.push(0);
+        break;
 
-    // template literal allows us to put variable values in the middle
-    // text
+      case 'squareTwo' :
+        display.innerText = 2;
+         if(displayStorageArray.at(-1) !== 'x' && displayStorageArray.at(-1) !== '-' && displayStorageArray.at(-1) !== "+" && displayStorageArray.at(-1) !== '/' &&  )
+          displayStorageArray.push(0);
+        break;
+
+      case 'squareThree':
+        if(displayStorageArray.at(-1) !== 'x' && displayStorageArray.at(-1) !== '-' && displayStorageArray.at(-1) !== "+" && displayStorageArray.at(-1) !== '/' &&  )
+        displayStorageArray.push(0);
+        display.innerText = 3;
+        break;
+
+      case 'squareFour':
+        if(displayStorageArray.at(-1) !== 'x' && displayStorageArray.at(-1) !== '-' && displayStorageArray.at(-1) !== "+" && displayStorageArray.at(-1) !== '/' &&  ){
+          displayStorageArray.push(0);
+          display.innerText = 4;
+        }
+        break;
+
+      case 'squareFive':
+        if(displayStorageArray.at(-1) !== 'x' && displayStorageArray.at(-1) !== '-' && displayStorageArray.at(-1) !== "+" && displayStorageArray.at(-1) !== '/' &&  ){
+          displayStorageArray.push(0);
+          display.innerText = 5;
+        }
+        break;
+
+      case 'squareSix':
+        if(displayStorageArray.at(-1) !== 'x' && displayStorageArray.at(-1) !== '-' && displayStorageArray.at(-1) !== "+" && displayStorageArray.at(-1) !== '/' &&  ){
+          displayStorageArray.push(0);
+          display.innerText = 6;
+        }
+        break;
+      
+      case 'squareSeven':
+        if(displayStorageArray.at(-1) !== 'x' && displayStorageArray.at(-1) !== '-' && displayStorageArray.at(-1) !== "+" && displayStorageArray.at(-1) !== '/' &&  ) {
+          displayStorageArray.push(0);
+        }
+            
+        }
+        display.innerText = 7;
+        break;
+ 
+      case 'squareEight':
+        if(displayStorageArray.at(-1) !== 'x' && displayStorageArray.at(-1) !== '-' && displayStorageArray.at(-1) !== "+" && displayStorageArray.at(-1) !== '/' &&  )
+          displayStorageArray.push(0);
+        display.innerText = 8;
+        break;
+ 
+      case 'squareNine':
+        if(displayStorageArray.at(-1) !== 'x' && displayStorageArray.at(-1) !== '-' && displayStorageArray.at(-1) !== "+" && displayStorageArray.at(-1) !== '/' &&  )
+          displayStorageArray.push(0);
+        display.innerText = 9;
+        break;
+ 
+      case 'squareMinus':
+        
+        display.innerText = '-';
+        break;
+ 
+      case 'squarePlus':
+        display.innerText = '+';
+        break;
+ 
+      case 'squareMultiply':
+        display.innerText = '0';
+        break;
+ 
+      case 'squareDivide':
+        display.innerText = '/';
+        break;
+
+      case 'square--C':
+        display.innerText = "";
+        displayStorageArray = [0];
+        console.log = [displayStorageArray]
+        break;
+
+    }
     
-    console.log("Click #" + i );
     if (square.id === "squareOne") {
       console.log(`The ${square.innerText} was clicked`);
     }
