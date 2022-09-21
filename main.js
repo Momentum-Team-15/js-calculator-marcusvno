12b8422b0849af1892279949edeccb79edfba293
Marcus Nogueira <marcusnogueira23@gmail.com>
2022-09-20 21:32:01 -0400

    Hover and tweaks on typography.

--- main.js
+++ main.js
@@ -1,11 +1,17 @@
 let squares = document.querySelectorAll(".square");
+let display = document.querySelector("#inputDisplay");
+
 // console.log(squares)
 // JS, go find all the elements with the class 'square'
 // querySelector only finds the first element
 // JS gets the literal chunks of HTML
 
 
-for (let square of squares) {
+
+
+
+
+for (let i; i< squares.length; i++) {
   // use a loop to apply the behavior to every square
   square.addEventListener("click", (event) => {
     // listen (wait) for the user to click and then do
@@ -13,12 +19,10 @@ for (let square of squares) {
 
     // template literal allows us to put variable values in the middle
     // text
-    square.classList.remove("purple");
-    square.classList.add("teal");
-    if (square.id === "square--one") {
+    
+    console.log("Click #" + i );
+    if (square.id === "squareOne") {
       console.log(`The ${square.innerText} was clicked`);
-      frame.style.backgroundImage = 'url("http://placekitten.com/300/300")';
-      console.log(frame.style.backgroundImage);
     }
   });
 }
